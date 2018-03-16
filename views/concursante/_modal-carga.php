17dd44c8b688ef4fadf25a0fa46c4a1b1d2cc877
Damián <damian2gom@gmail.com>
2018-03-16 13:44:55 -0600

    Modales (acomodo y css)

--- views/concursante/_modal-carga.php
+++ views/concursante/_modal-carga.php
@@ -9,12 +9,14 @@ use yii\bootstrap\Modal;
       ],
     ]);
 
-    echo 'Publica diariamente tus 3 fotografías:
-    Foto de producto en una mesa del restaurante.
-    Foto de montaje de la barra con el producto presente.
-    Foto de los comensales degustando el producto.
-    
-    Únicamente participan las fotografías publicadas dentro del horario de 11:00 am a 11:00 pm Las mejores fotos que se publiquen primero tienen más oportunidades de ganar.';
+    echo '<h4>Publica diariamente tus 3 fotografías:</h4>
+      <ul>
+        <li>Foto de producto en una mesa del restaurante.</li>
+        <li>Foto de montaje de la barra con el producto presente.</li>
+        <li>Foto de los comensales degustando el producto.</li>
+      </ul>
+      <p class="text-italic">Únicamente participan las fotografías publicadas dentro del horario de 11:00 am a 11:00 pm Las mejores fotos que se publiquen primero tienen más oportunidades de ganar.</p>
+    ';
 
     Modal::end();
     ?>
